9a5e13692134540cdb2ee1a695afb1dbbcb439da
linkla <josephdero@gmail.com>
2020-02-20 12:57:02 -0300

    configuring buildspec.yml

--- build/app.js
+++ build/app.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.get("/", function (req, res) {
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.listen(3000, function () {
